3fb8f45c9e65e6cbe902da1b1cfda0f66d49d196
haniev <haniev@yandex-team.ru>
2020-08-16 18:00:13 +0300

    Чистка

--- src/cdx-util/index.js
+++ src/cdx-util/index.js
@@ -1,41 +1,7 @@
 const Logging = require('./logging');
-const Rating = require('./rating');
-const Tick = require('./tick');
-const Converter = require('./converter');
-const queue = require('./queue');
-const HistoricalRates = require('./historical-rates');
-const SimpleCache = require('./simple-cache');
-const following = require('./following');
-const MailSender = require('./mail');
-const sentry = require('./sentry');
-const ds = require('./ds');
-const cluster = require('./cluster');
-const FutureProfits = require('./future-profits');
-
 const util = require('util');
 const request = util.promisify(require('request'));
-
-
 const { envFlag, envRequire } = require('./env');
-const {
-  sum,
-  hash,
-  choose,
-  groupByGap,
-  restGetURL,
-  buildArgs,
-  filteredPairs,
-  filterObject,
-  randomToken,
-  sha256,
-  stableStringify,
-  JSONHash,
-  mod,
-} = require('./common');
-
-const NotImplementedError = () => new Error('Method not implemented');
-
-const hideCredential = str => `${str.slice(0, 2)}...${str.slice(-2)}`;
 
 class UserError extends Error {
   constructor(...args) {
@@ -158,38 +124,40 @@ module.exports = {
   sendNotificationToPhoneAdmin,
   sendNotificationToUser,
   sendTelegramMessageToAdmin,
-
-  randomToken,
   envFlag,
   envRequire,
-  NotImplementedError,
   Logging,
-  Rating,
-  Tick,
-  sentry,
-  queue,
-  MailSender,
-  hideCredential,
-  sum,
-  hash,
-  choose,
-  groupByGap,
-  HistoricalRates,
-  SimpleCache,
-  restGetURL,
-  buildArgs,
-  filteredPairs,
-  filterObject,
-  following,
   UserResponse,
   UserError,
   UserResponseOK,
-  ds,
-  cluster,
-  sha256,
-  stableStringify,
-  JSONHash,
-  mod,
-  Converter,
-  FutureProfits,
+
+  // randomToken,
+  
+  // NotImplementedError,
+  // Rating,
+  // Tick,
+  // sentry,
+  // queue,
+  // MailSender,
+  // hideCredential,
+  // sum,
+  // hash,
+  // choose,
+  // groupByGap,
+  // HistoricalRates,
+  // SimpleCache,
+  // restGetURL,
+  // buildArgs,
+  // filteredPairs,
+  // filterObject,
+  // following,
+  
+  // ds,
+  // cluster,
+  // sha256,
+  // stableStringify,
+  // JSONHash,
+  // mod,
+  // Converter,
+  // FutureProfits,
 };
--- src/cdx-util/sentry.js
+++
@@ -1,6 +0,0 @@
-const Sentry = require('@sentry/node');
-
-module.exports = {
-  init: config => Sentry.init(config.sentry),
-  captureException: err => Sentry.captureException(err),
-};
