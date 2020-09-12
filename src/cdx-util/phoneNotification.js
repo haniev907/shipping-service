3a0d3e9882a5ba58e6dcf792ff08f514aa8426be
haniev907 <35073345+haniev907@users.noreply.github.com>
2020-09-12 22:27:13 +0300

    Отключил смс нотификации пользователям

--- src/cdx-util/phoneNotification.js
+++ src/cdx-util/phoneNotification.js
@@ -1,7 +1,13 @@
 const SmsRu = require('sms_ru');
 const smsClient = new SmsRu('BB0FE51A-85B4-3EFE-C3C6-F69AE01C6C6A');
 
+const enabledSmsNot = false;
+
 const sendNotificationToUser = (phone, text) => {
+  if (!enabledSmsNot) {
+    return;
+  }
+
   smsClient.sms_send({to: phone, text: text || 'Получен новый заказ'}, (e) => {
     console.log(e.description);
   });
