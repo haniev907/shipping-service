19e19c2531f546132be0d84b90c561d015ee14c3
haniev907 <35073345+haniev907@users.noreply.github.com>
2022-08-16 21:37:12 +0300

    Update controllers.js

--- services/api/router/user/controllers.js
+++ services/api/router/user/controllers.js
@@ -11,7 +11,7 @@ const collect = (config, cdx) => {
     addRestaurant: async (req, res) => {
       const {
         userId, body: {
-          name, address, photo, telegramChatId, city, onlinePayMessage
+          name, address, photo, telegramChatId, city, onlinePayMessage, customId
         },
       } = req;
 
@@ -19,7 +19,7 @@ const collect = (config, cdx) => {
         const pathArr = filepath.split('/')
         const fileName = pathArr[pathArr.length - 1];
 
-        await cdx.db.restaurant.createRestaurant({name, address, photo: fileName, userId, telegramChatId, city, onlinePayMessage});
+        await cdx.db.restaurant.createRestaurant({name, address, photo: fileName, userId, telegramChatId, city, onlinePayMessage, customId});
 
         res.json(new cdxUtil.UserResponseOK());
       });
@@ -28,7 +28,7 @@ const collect = (config, cdx) => {
     editRestaurant: async (req, res) => {
       const {
         userId, body: {
-          name, address, photo, telegramChatId, idRest, city, onlinePayMessage
+          name, address, photo, telegramChatId, idRest, city, onlinePayMessage, customId
         },
       } = req;
 
@@ -38,7 +38,7 @@ const collect = (config, cdx) => {
           const fileName = pathArr[pathArr.length - 1];
   
           await cdx.db.restaurant.editRestaurant(idRest, {
-            name, address, photo: fileName, telegramChatId, city, onlinePayMessage
+            name, address, photo: fileName, telegramChatId, city, onlinePayMessage, customId
           });
   
           res.json(new cdxUtil.UserResponseOK());
